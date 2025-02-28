3301375a48480fa17580df7f7800a9f426751dcf
Greg Magarshak <greg@qbix.com>
2025-02-27 22:47:49 -0500

    Greatly improved hints

--- platform/plugins/Q/web/js/methods/Q/Onboarding/start.js
+++ platform/plugins/Q/web/js/methods/Q/Onboarding/start.js
@@ -7,71 +7,131 @@ Q.exports(function (Q) {
      */
 
     /**
-     * Starts an onboarding process
+     * Starts an onboarding process. You can also trigger
+     * Q.Onboarding.event manually with Q.handle().
      * @static
      * @method start
-     * @param {String} [key] The key under which to store Q.Onboarding.processes
+     * @param {Object} [instructions] An object of key: instructionObject pairs
+     *   where instructionObject has at least "appear" key, with value = CSS selector.
+     *   It can also have optional:
+     *   "targets" (selector to override what to hint),
+     *   "disappear" (selector to override what should disappear before hint event stops)
+     *   "after" (key of a hint event that has to stop before this hint starts)
+     *   "options" (for options passed to Q.Visual.hint, merged over Q.Onboarding.options.hint)
+     * 
      */
     return function _Q_Onboarding_start(instructions) {
         if (!instructions) {
             return false;
         }
-        var selectors = Q.Onboarding.selectors = {};
-        var events = Q.Onboarding.events = {};
-        var appeared = {};
+        if (typeof Q.Onboarding.textName === 'string') {
+            Q.Text.get(Q.Onboarding.textName).then(function (text) {
+                Q.Onboarding.text = text;
+                if (!Q.isEmpty(Q.Onboarding.textPath)) {
+                    Q.Onboarding.text = Q.getObject(
+                        Q.Onboarding.textPath, Q.Onboarding.text
+                    );
+                }
+            });
+        }
+        var selectors = Q.Onboarding.selectors;
+        var events = Q.Onboarding.events;
+        var waitToDisappear = Q.Onboarding.waitToDisappear = {};
         Q.each(instructions, function (k) {
             var instr = instructions[k];
             events[k] = new Q.Event(function (targets, k) {
+                var hintTargets = (selectors[k].targets)
+                    ? document.querySelectorAll(selectors[k].targets)
+                    : targets;
                 var lsk = Q.Onboarding.prefix + k;
                 if (localStorage[lsk]) {
-                    events[k].stop(); // mark it as already happened
+                    events[k].stop(); // it already occurred before
                 } else {
-                    Q.Visual.hint(targets, instr.options);
-                    localStorage[lsk] = 1;
+                    var o = Q.extend({}, Q.Onboarding.options.hint, instr.options);
+                    if (Q.Onboarding.text[k]) {
+                        o.tooltip = Q.extend({
+                            text: Q.Onboarding.text && Q.Onboarding.text[k]
+                        }, o.tooltip);
+                    }
+                    o.onHide = Q.extend(o.onHide, {
+                        'Q.Onboarding': function () {
+                            setTimeout(function () {
+                                if (!waitToDisappear[k]) {
+                                    // They never appeared, or weren't speciied
+                                    // So go to the next hint.
+                                    localStorage[Q.Onboarding.prefix + k] = 'stopped';
+                                    events[k].stop();
+                                    return;
+                                }
+                            }, Q.Onboarding.interval.afterHintHide || 3000);
+                        }
+                    });
+                    Q.Visual.stopHints(); // stop previous hints
+                    Q.Visual.hint(hintTargets, o);
+                    localStorage[lsk] = 'occurred';
                 }
             }, 'Q.hint');
             selectors[k] = { appear: instr.appear };
-            if (instr.untilDisappear) {
-                selectors[k].disappear = instr.untilDisappear;
-            }
+            Q.take(instr, ['targets', 'disappear'], selectors[k]);
         });
-        instructions.intervalId = setInterval(function () {
+        if (Q.Onboarding.intervalId) {
+            return;
+        }
+        Q.Onboarding.intervalId = setInterval(function () {
             for (var k in selectors) {
                 var selectorToAppear = selectors[k].appear;
                 var selectorToDisappear = selectors[k].disappear || selectors[k].appear;
                 var elementsToAppear = document.querySelectorAll(selectorToAppear);
-                var targets = [];
+                var appeared = [];
                 var visible = false;
                 for (var i=0; i<elementsToAppear.length; ++i) {
                     var r = elementsToAppear[i].getBoundingClientRect();
                     if (r.width && r.height) {
                         visible = true;
-                        targets.push(elementsToAppear[i]);
+                        appeared.push(elementsToAppear[i]);
                     }
                 }
                 if (!events[k].occurred && visible) {
                     var after = instructions[k].after;
-                    if (!after || Q.Onboarding.events[after].stopped) {
-                        events[k].handle.call(events[k], targets, k);
+                    if (typeof after === 'string') {
+                        after = [after];
+                    }
+                    var proceed = false;
+                    Q.each(after, function (i, s) {
+                        if (Q.Onboarding.events[s].stopped
+                        || localStorage[Q.Onboarding.prefix + s] === 'stopped') {
+                            proceed = true;
+                            return false;
+                        }
+                    });
+                    if (!after || proceed) {
+                        Q.Onboarding.waitToDisappear[k] = false;
+                        Q.handle(events[k], instructions, [appeared, k]);
                     }
                 }
-                if (selectorToDisappear !== selectorToAppear) {
-                    visible = false;
-                    var elementsToDisappear = document.querySelectorAll(selectorToDisappear);
-                    for (var i=0; i<elementsToDisappear.length; ++i) {
-                        var r = elementsToDisappear[i].getBoundingClientRect();
-                        if (r.width && r.height) {
-                            visible = true; 
-                            break;
-                        }
+                if (!events[k].occurred || events[k].stopped) {
+                    continue;
+                }
+                if (selectorToDisappear === selectorToAppear) {
+                    events[k].stop(); // nothing to wait for
+                    continue;
+                }
+                visible = false;
+                var elementsToDisappear = document.querySelectorAll(selectorToDisappear);
+                for (var i=0; i<elementsToDisappear.length; ++i) {
+                    var r = elementsToDisappear[i].getBoundingClientRect();
+                    if (r.width && r.height) {
+                        visible = true; 
+                        break;
                     }
                 }
                 if (visible) {
-                    appeared[k] = true;
+                    waitToDisappear[k] = true;
                 }
-                if (events[k].occurred && !events[k].stopped
-                && appeared[k] && !visible) {
+                if (waitToDisappear[k] && !visible) {
+                    localStorage[Q.Onboarding.prefix + k] = 'stopped';
                     events[k].stop();
+                    waitToDisappear[k] = false;
                 }
             }
         }, 100);
