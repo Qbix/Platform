0c56d0a4b1913b79f5599f775c82bffaa29f4cbe
Greg Magarshak <greg@qbix.com>
2025-10-29 07:43:52 -0400

    Made DB/Row.php compatible with PHP up to 7

--- platform/classes/Db/Row.php
+++ platform/classes/Db/Row.php
@@ -581,7 +581,7 @@ class Db_Row
 		$relationName, 
 		$to_class_name, 
 		$alias, 
-		Db_Relation $relation, /*Db_Relation relation2, etc. */ )
+		Db_Relation $relation /*Db_Relation relation2, etc. */ )
 	{
 		$args = func_get_args();
 		array_unshift($args, get_class($this));
@@ -607,7 +607,7 @@ class Db_Row
 		$relationName, 
 		$to_class_name, 
 		$alias, 
-		Db_Relation $relation, /*Db_Relation relation2, etc. */ )
+		Db_Relation $relation /* Db_Relation relation2, etc. */ )
 	{
 		$args = func_get_args();
 		$count = count($args);
@@ -645,7 +645,7 @@ class Db_Row
 		$relationName, 
 		$to_class_name,
 		$from_alias, 
-		Db_Relation $relation, /*Db_Relation relation2, etc. */ )
+		Db_Relation $relation /*Db_Relation relation2, etc. */ )
 	{
 		$args = func_get_args();
 		$count = count($args);
