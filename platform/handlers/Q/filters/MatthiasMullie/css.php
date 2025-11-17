2d3a1bcd000f2952c0d6aedb3813acb6919dd3b1
Greg Magarshak <greg@qbix.com>
2025-11-17 00:39:48 -0500

    Fixed combine.php hook implementations

--- platform/handlers/Q/filters/MatthiasMullie/css.php
+++ platform/handlers/Q/filters/MatthiasMullie/css.php
@@ -4,22 +4,34 @@ function Q_filters_MatthiasMullie_css($params)
 {
 	$processed = $params['processed'];
 	$results = array();
-	$printProgress = Q::ifset($params, 'printProgress', false);
 	$filters = array();
+	$printProgress = Q::ifset($params, 'printProgress', false);
+
 	foreach ($processed as $src => $part) {
 		if ($printProgress) {
 			echo "\tQ_filters_MatthiasMullie_css: $src" . PHP_EOL;
 		}
+
+		$minified = null;
+
 		try {
 			$minify = new MatthiasMullie\Minify\CSS($part);
+			$minified = $minify->minify();
 		} catch (Exception $e) {
+			// Fall back to unminified CSS
+			$minified = $part;
 
+			if ($printProgress) {
+				echo "\t\t[warning] CSS minify failed for $src: " . $e->getMessage() . PHP_EOL;
+			}
 		}
-		$results[$src] = $minify->minify();
+
+		$results[$src] = $minified;
 		$filters[$src] = 'Q/filters/MatthiasMullie/css';
 	}
 
 	$output = implode("\n\n", $results);
+
 	$params['info']['output'] = $output;
 	$params['info']['results'] = $results;
 	$params['info']['filters'] = $filters;
--- platform/handlers/Q/filters/MatthiasMullie/js.php
+++ platform/handlers/Q/filters/MatthiasMullie/js.php
@@ -3,22 +3,34 @@
 function Q_filters_MatthiasMullie_js($params)
 {
 	$results = array();
-	$printProgress = Q::ifset($params, 'printProgress', false);
 	$filters = array();
+	$printProgress = Q::ifset($params, 'printProgress', false);
+
 	foreach ($params['parts'] as $src => $part) {
 		if ($printProgress) {
 			echo "\tQ_filters_MatthiasMullie_js: $src" . PHP_EOL;
 		}
+
+		$minified = null;
+
 		try {
 			$minify = new MatthiasMullie\Minify\JS($part);
+			$minified = $minify->minify();
 		} catch (Exception $e) {
+			// Fall back to unminified content
+			$minified = $part;
 
+			if ($printProgress) {
+				echo "\t\t[warning] JS minify failed for $src: " . $e->getMessage() . PHP_EOL;
+			}
 		}
-		$results[$src] = $minify->minify();
+
+		$results[$src] = $minified;
 		$filters[$src] = 'Q/filters/MatthiasMullie/js';
 	}
 
 	$output = implode("\n\n", $results);
+
 	$params['info']['output'] = $output;
 	$params['info']['results'] = $results;
 	$params['info']['filters'] = $filters;
