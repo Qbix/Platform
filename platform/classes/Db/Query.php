370ba31c0311c473ab66aaf74fe98fca1a533a5a
Greg Magarshak <greg@qbix.com>
2026-01-14 18:37:00 -0500

    Updated Query SELECT with empty fields

--- platform/classes/Db/Query.php
+++ platform/classes/Db/Query.php
@@ -1296,7 +1296,7 @@ abstract class Db_Query extends Db_Expression
 			throw new Exception("The fields to select need to be specified correctly.", -1);
 		}
 
-		if (empty($this->clauses['SELECT'])) {
+		if (!isset($this->clauses['SELECT'])) {
 			$this->clauses['SELECT'] = $fields;
 		} else {
 			$this->clauses['SELECT'] .= ", $fields";
